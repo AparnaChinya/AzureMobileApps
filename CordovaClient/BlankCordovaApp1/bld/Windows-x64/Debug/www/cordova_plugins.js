3db6842b324869f73fb94e880ca48563d25fc6b5
Aparna Chinya <apchin@outlook.com>
2016-09-06 19:52:28 +0530

    Adding Cordova client - PushNotification for Windows

---
+++ CordovaClient/BlankCordovaApp1/bld/Windows-x64/Debug/www/cordova_plugins.js
@@ -0,0 +1,102 @@
+﻿cordova.define('cordova/plugin_list', function(require, exports, module) {
+module.exports = [
+    {
+        "file": "plugins/cordova-plugin-inappbrowser/www/inappbrowser.js",
+        "id": "cordova-plugin-inappbrowser.inappbrowser",
+        "pluginId": "cordova-plugin-inappbrowser",
+        "clobbers": [
+            "cordova.InAppBrowser.open",
+            "window.open"
+        ]
+    },
+    {
+        "file": "plugins/cordova-plugin-inappbrowser/src/windows/InAppBrowserProxy.js",
+        "id": "cordova-plugin-inappbrowser.InAppBrowserProxy",
+        "pluginId": "cordova-plugin-inappbrowser",
+        "merges": [
+            ""
+        ]
+    },
+    {
+        "file": "plugins/cordova-sqlite-storage/www/SQLitePlugin.js",
+        "id": "cordova-sqlite-storage.SQLitePlugin",
+        "pluginId": "cordova-sqlite-storage",
+        "clobbers": [
+            "SQLitePlugin"
+        ]
+    },
+    {
+        "file": "plugins/cordova-sqlite-storage/src/windows/sqlite-proxy.js",
+        "id": "cordova-sqlite-storage.SQLiteProxy",
+        "pluginId": "cordova-sqlite-storage",
+        "merges": [
+            ""
+        ]
+    },
+    {
+        "file": "plugins/cordova-sqlite-storage/src/windows/SQLite3-Win-RT/SQLite3JS/js/SQLite3.js",
+        "id": "cordova-sqlite-storage.SQLite3",
+        "pluginId": "cordova-sqlite-storage",
+        "merges": [
+            ""
+        ]
+    },
+    {
+        "file": "plugins/cordova-plugin-ms-azure-mobile-apps/www/MobileServices.Cordova.Ext.js",
+        "id": "cordova-plugin-ms-azure-mobile-apps.AzureMobileServices.Ext",
+        "pluginId": "cordova-plugin-ms-azure-mobile-apps",
+        "runs": true
+    },
+    {
+        "file": "plugins/cordova-plugin-ms-azure-mobile-apps/www/MobileServices.Cordova.js",
+        "id": "cordova-plugin-ms-azure-mobile-apps.AzureMobileServices",
+        "pluginId": "cordova-plugin-ms-azure-mobile-apps",
+        "clobbers": [
+            "WindowsAzure"
+        ]
+    },
+    {
+        "file": "plugins/cordova-plugin-device/www/device.js",
+        "id": "cordova-plugin-device.device",
+        "pluginId": "cordova-plugin-device",
+        "clobbers": [
+            "device"
+        ]
+    },
+    {
+        "file": "plugins/cordova-plugin-device/src/windows/DeviceProxy.js",
+        "id": "cordova-plugin-device.DeviceProxy",
+        "pluginId": "cordova-plugin-device",
+        "merges": [
+            ""
+        ]
+    },
+    {
+        "file": "plugins/phonegap-plugin-push/www/push.js",
+        "id": "phonegap-plugin-push.PushNotification",
+        "pluginId": "phonegap-plugin-push",
+        "clobbers": [
+            "PushNotification"
+        ]
+    },
+    {
+        "file": "plugins/phonegap-plugin-push/src/windows/PushPluginProxy.js",
+        "id": "phonegap-plugin-push.PushPlugin",
+        "pluginId": "phonegap-plugin-push",
+        "merges": [
+            ""
+        ]
+    }
+];
+module.exports.metadata = 
+// TOP OF METADATA
+{
+    "cordova-plugin-whitelist": "1.2.2",
+    "cordova-plugin-inappbrowser": "1.4.0",
+    "cordova-sqlite-storage": "1.4.7",
+    "cordova-plugin-ms-azure-mobile-apps": "2.0.0-rc1",
+    "cordova-plugin-device": "1.1.2",
+    "phonegap-plugin-push": "1.8.1"
+};
+// BOTTOM OF METADATA
+});
---
+++ CordovaClient/BlankCordovaApp1/www/scripts/index.js
@@ -0,0 +1,159 @@
+﻿// For an introduction to the Blank template, see the following documentation:
+// http://go.microsoft.com/fwlink/?LinkID=397704
+// To debug code on page load in Ripple or on Android devices/emulators: launch your app, set breakpoints, 
+// and then run "window.location.reload()" in the JavaScript Console.
+(function () {
+    "use strict";
+    var client,             // Connection to the Azure Mobile App backend
+        todoItemTable;
+    document.addEventListener( 'deviceready', onDeviceReady.bind( this ), false );
+
+    function onDeviceReady() {
+        // Handle the Cordova pause and resume events
+        document.addEventListener( 'pause', onPause.bind( this ), false );
+        document.addEventListener( 'resume', onResume.bind( this ), false );
+        
+        client = new WindowsAzure.MobileServiceClient("https://apchin-Mobileapp.azurewebsites.net");
+        todoItemTable = client.getTable("todoitem");
+
+        $('#add-item').submit(addItemHandler);
+        $('#refresh').on('click', refreshDisplay);
+        registerForPushNotifications();
+      
+    };
+
+    // Register for Push Notifications. Requires that phonegap-plugin-push be installed.
+    var pushRegistration = null;
+    function registerForPushNotifications() {
+        pushRegistration = PushNotification.init({
+            windows: {}
+        });
+
+        // Handle the registration event.
+        pushRegistration.on('registration', function (data) {
+            // Get the native platform of the device.
+            var platform = device.platform;
+            // Get the handle returned during registration.
+            var handle = data.registrationId;
+            // Set the device-specific message template.
+        if (device.platform === 'windows') {
+                // Register for WNS notifications.
+                client.push.register('wns', handle, {
+                    myTemplate: {
+                        body: '<toast><visual><binding template="ToastText01"><text id="1">$(messageParam)</text></binding></visual></toast>',
+                        headers: { 'X-WNS-Type': 'wns/toast' }
+                    }
+                });
+            }
+        });
+
+        pushRegistration.on('notification', function (data, d2) {
+            alert('Push Received: ' + data.message);
+        });
+
+        pushRegistration.on('error', handleError);
+    }
+
+    function refreshDisplay() {
+        updateSummaryMessage('Loading Data from Azure');
+
+        // Execute a query for uncompleted items and process
+        todoItemTable
+            .where({ complete: false })     // Set up the query
+            .read()                         // Read the results
+            .then(createTodoItemList, handleError);
+    }
+
+
+    function updateSummaryMessage(msg) {
+        $('#summary').html(msg);
+    }
+
+
+    function createTodoItem(item) {
+        return $('<li>')
+            .attr('data-todoitem-id', item.id)
+            .append($('<button class="item-delete">Delete</button>'))
+            .append($('<input type="checkbox" class="item-complete">').prop('checked', item.complete))
+            .append($('<div>').append($('<input class="item-text">').val(item.text)));
+    }
+
+
+    function createTodoItemList(items) {
+        // Cycle through each item received from Azure and add items to the item list
+        var listItems = $.map(items, createTodoItem);
+        $('#todo-items').empty().append(listItems).toggle(listItems.length > 0);
+        $('#summary').html('<strong>' + items.length + '</strong> item(s)');
+
+        // Wire up the event handlers for each item in the list
+        $('.item-delete').on('click', deleteItemHandler);
+        $('.item-text').on('change', updateItemTextHandler);
+        $('.item-complete').on('change', updateItemCompleteHandler);
+    }
+
+    function handleError(error) {
+        var text = error + (error.request ? ' - ' + error.request.status : '');
+        console.error(text);
+        $('#errorlog').append($('<li>').text(text));
+    }
+
+    function getTodoItemId(el) {
+        return $(el).closest('li').attr('data-todoitem-id');
+    }
+
+
+    function addItemHandler(event) {
+        var textbox = $('#new-item-text'),
+            itemText = textbox.val();
+
+        updateSummaryMessage('Adding New Item');
+        if (itemText !== '') {
+            todoItemTable.insert({
+                text: itemText,
+                complete: false
+            }).then(refreshDisplay, handleError);
+        }
+
+        textbox.val('').focus();
+        event.preventDefault();
+    }
+
+    function deleteItemHandler(event) {
+        var itemId = getTodoItemId(event.currentTarget);
+
+        updateSummaryMessage('Deleting Item in Azure');
+        todoItemTable
+            .del({ id: itemId })   // Async send the deletion to backend
+            .then(refreshDisplay, handleError); // Update the UI
+        event.preventDefault();
+    }
+
+    function updateItemTextHandler(event) {
+        var itemId = getTodoItemId(event.currentTarget),
+            newText = $(event.currentTarget).val();
+
+        updateSummaryMessage('Updating Item in Azure');
+        todoItemTable
+            .update({ id: itemId, text: newText })  // Async send the update to backend
+            .then(refreshDisplay, handleError); // Update the UI
+        event.preventDefault();
+    }
+
+
+    function updateItemCompleteHandler(event) {
+        var itemId = getTodoItemId(event.currentTarget),
+            isComplete = $(event.currentTarget).prop('checked');
+
+        updateSummaryMessage('Updating Item in Azure');
+        todoItemTable
+            .update({ id: itemId, complete: isComplete })  // Async send the update to backend
+            .then(refreshDisplay, handleError);        // Update the UI
+    }
+    function onPause() {
+        // TODO: This application has been suspended. Save application state here.
+    };
+
+    function onResume() {
+        // TODO: This application has been reactivated. Restore application state here.
+    };
+} )();
